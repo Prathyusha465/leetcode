82427b0d9b6b5a87434b4ed662bd447b67077dd4
Prathyusha465 <121740500+Prathyusha465@users.noreply.github.com>
2023-02-14 15:26:00 +0530

    Time: 31 ms (13.81%), Space: 144.7 MB (5.57%) - LeetHub

---
+++ 0415-add-strings/0415-add-strings.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    string addStrings(string num1, string num2) {
+        string ans="";
+        int x=num1.size()-1;
+        int y=num2.size()-1;
+        int carry=0;
+        while(x>=0 || y>=0 || carry)
+        {
+            int temp=0;
+            if(x>=0)
+            {
+              carry+=num1[x]-'0';
+               x--;
+            }
+            if(y>=0)
+            {
+              carry+=num2[y]-'0';
+               y--;
+            }
+       ans=ans+char(carry%10+'0');
+       carry=carry/10;
+
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
