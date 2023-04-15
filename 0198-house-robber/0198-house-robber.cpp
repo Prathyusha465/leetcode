776fe2e2754516c27ac4b55dba9078f470b95c40
Prathyusha465 <121740500+Prathyusha465@users.noreply.github.com>
2023-04-15 21:35:39 +0530

    Time: 3 ms (34.87%), Space: 7.9 MB (35.91%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    /*
     int helper(vector<int>& nums,int i,vector<int>&dp)
     {
         if(i<0)
@@ -10,10 +11,18 @@ public:
             pick= nums[i]+helper(nums,i-2,dp);
         int npick=helper(nums,i-1,dp);
         return dp[i]=max(pick,npick);
-    }
+    }*/
     int rob(vector<int>& nums) {
         int n=nums.size();
-        vector<int>dp(n,-1);
-       return helper(nums,n-1,dp);
+        vector<int>dp(n,0);
+       dp[0]=nums[0];
+        for(int i=1;i<n;i++)
+        {   
+            if(i==1)
+                dp[i]=max(nums[0],nums[1]);
+            if(i>1)
+            dp[i]=max(nums[i]+dp[i-2],dp[i-1]);
+        }
+        return dp[n-1];
     }
 };
