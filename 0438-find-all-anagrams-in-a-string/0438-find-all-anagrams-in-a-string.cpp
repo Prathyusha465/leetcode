9753bcbb04ccb21fd768a0f31de752774f877757
Prathyusha465 <121740500+Prathyusha465@users.noreply.github.com>
2024-03-26 11:19:11 +0530

    Time: 40 ms (15.72%), Space: 18.3 MB (9.84%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
@@ -10,17 +10,19 @@ public:
         int i=0;
         int j=0;
         while(j<n){
-            m2[s[j]]++;
+           m2[s[j]]++;
             if(j-i+1 == m){
-                if(m1==m2)
-                  ans.push_back(i);
+                if(m1==m2){
+                   ans.push_back(i); 
+                }
                 m2[s[i]]--;
                 if(m2[s[i]]==0)
                     m2.erase(s[i]);
                 i++;
             }
             j++;
-        }  
+        } 
+      
         return ans;
     }
 };
