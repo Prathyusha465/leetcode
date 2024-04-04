3d52ac599dae3aca1cd090cce626329a88c97066
Prathyusha465 <121740500+Prathyusha465@users.noreply.github.com>
2024-04-04 21:48:21 +0530

    Time: 122 ms (7.44%), Space: 25.6 MB (58.67%) - LeetHub

--- 0187-repeated-dna-sequences/0187-repeated-dna-sequences.cpp
+++ 0187-repeated-dna-sequences/0187-repeated-dna-sequences.cpp
@@ -4,16 +4,15 @@ public:
         int n=s.size();
         map<string,int>m1;
         vector<string>ans;
-        int i=0;
-       int j=10;
-       while(i+10 <=n){
-         string temp= s.substr(i,10);
+        int j=0;
+       while(j+10 <=n){
+         string temp= s.substr(j,10);
          if(m1.find(temp)!=m1.end()){
                 if(m1[temp]==1)
                     ans.push_back(temp);
              }
            m1[temp]++;
-           i++;
+           j++;
        }
         return ans;
     }
